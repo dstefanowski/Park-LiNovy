9c262ddcb393404a5a2f6388a6193b5c9573d3dd
Dominik Stefanowski <dstefanowski@gmail.com>
2023-04-30 12:28:18 +0200

    nav-desktop in works

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,14 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
 
-  if (currentScrollPos < 80) {
-    document.querySelector(".navbar").classList.remove("scroll-down");
+  if (currentScrollPos < 350) {
+    document.querySelector(".nav-desktop").classList.remove("scroll-down");
   } else if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".navbar1").classList.remove("scroll-down");
-    document.querySelector(".navbar1").classList.add("scroll-up");
+    document.querySelector(".nav-desktop").classList.remove("scroll-down");
+    document.querySelector(".nav-desktop").classList.add("scroll-up");
   } else {
-    document.querySelector(".navbar1").classList.remove("scroll-up");
-    document.querySelector(".navbar1").classList.add("scroll-down");
+    document.querySelector(".nav-desktop").classList.remove("scroll-up");
+    document.querySelector(".nav-desktop").classList.add("scroll-down");
   }
   prevScrollpos = currentScrollPos;
 };
@@ -33,28 +33,5 @@ const handleNav = () => {
 
 burgerBtn.addEventListener("click", handleNav);
 
-// Alert i kopiowanie treści do schowka
-// function showAlert(message, duration) {
-// 	const alertBox = document.createElement('div')
-// 	alertBox.classList.add('alert')
-// 	alertBox.textContent = message
-// 	document.body.appendChild(alertBox)
-// 	setTimeout(function () {
-// 		alertBox.remove()
-// 	}, duration)
-// }
-
-// function copyToClipboard(elementId) {
-// 	const copyText = document.getElementById(elementId).textContent
-// 	navigator.clipboard
-// 		.writeText(copyText)
-// 		.then(() => {
-// 			// confirm('Tekst został skopiowany do schowka.')
-// 			showAlert('Kod produktu skopiowany do schowka!', 1500)
-// 		})
-// 		.catch(err => {
-// 			console.error('Wystąpił błąd podczas kopiowania do schowka: ', err)
-// 		})
-// }
-
+// ===========================================================================
 let rellax = new Rellax(".rellax", {});
