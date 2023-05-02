bc9369a73918beed410ef82e586a9b2fa312e8c2
Dominik Stefanowski <dstefanowski@gmail.com>
2023-05-02 10:57:34 +0200

    nav-desktop behavior changed - scroll-reveal off; color-swap on

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,38 @@
 // alert('Kod JS podpięty prawidłowo')
+// =============================================================================
+// Nav-desktop color change
 
-// Navbar - Scroll-reveal
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
+const navDesktop = document.querySelector(".nav-desktop");
+
+window.onscroll = () => {
   let currentScrollPos = window.pageYOffset;
 
-  if (currentScrollPos < 350) {
-    document.querySelector(".nav-desktop").classList.remove("scroll-down");
-  } else if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav-desktop").classList.remove("scroll-down");
-    document.querySelector(".nav-desktop").classList.add("scroll-up");
+  if (currentScrollPos > 768) {
+    navDesktop.classList.add("bg-dark");
   } else {
-    document.querySelector(".nav-desktop").classList.remove("scroll-up");
-    document.querySelector(".nav-desktop").classList.add("scroll-down");
+    navDesktop.classList.remove("bg-dark");
   }
-  prevScrollpos = currentScrollPos;
 };
+// =============================================================================
+
+// Navbar - Scroll-reveal
+// let prevScrollpos = window.pageYOffset;
+
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
+
+//   if (currentScrollPos < 500) {
+//     document.querySelector(".nav-desktop").classList.remove("scroll-down");
+//   } else if (prevScrollpos > currentScrollPos) {
+//     document.querySelector(".nav-desktop").classList.remove("scroll-down");
+//     document.querySelector(".nav-desktop").classList.add("scroll-up");
+//   } else {
+//     document.querySelector(".nav-desktop").classList.remove("scroll-up");
+//     document.querySelector(".nav-desktop").classList.add("scroll-down");
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+// =============================================================================
 
 // Burger button - MENU
 const nav = document.querySelector(".nav");
@@ -35,3 +52,5 @@ burgerBtn.addEventListener("click", handleNav);
 
 // ===========================================================================
 let rellax = new Rellax(".rellax", {});
+
+// ===========================================================================
