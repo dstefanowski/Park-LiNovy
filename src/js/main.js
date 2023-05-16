2c9b153469018919a132ace867de7028bda3cd2d
Dominik Stefanowski <dstefanowski@gmail.com>
2023-05-16 14:50:05 +0200

    phone number and email address actions added

--- src/js/main.js
+++ src/js/main.js
@@ -82,3 +82,33 @@ $(document).ready(function () {
 });
 // ===============================================================
 
+// function copyToClipboard(text) {
+//   navigator.clipboard.writeText(text);
+//   alert("Adres e-mail skopiowany do schowka!");
+
+// }
+
+
+function showAlert(message, duration) {
+  var alertDiv = document.createElement('div');
+  alertDiv.innerText = message;
+  alertDiv.style.position = 'fixed';
+  alertDiv.style.top = '10px';
+  alertDiv.style.left = '50%';
+  alertDiv.style.transform = 'translateX(-50%)';
+  alertDiv.style.padding = '10px';
+  alertDiv.style.backgroundColor = 'lightblue';
+  alertDiv.style.border = '1px solid gray';
+  alertDiv.style.borderRadius = '5px';
+  alertDiv.style.zIndex = '9999';
+  document.body.appendChild(alertDiv);
+  
+  setTimeout(function() {
+    alertDiv.remove();
+  }, duration);
+}
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text);
+  showAlert('Adres e-mail skopiowany do schowka!', 2000); // 2000 ms = 2 sekundy
+}
